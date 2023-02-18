ba44a6cdc1e417e608bd662723ece3b2e401462e
Paul Simkonis <122229125+SpruceWoodTree@users.noreply.github.com>
2023-02-18 17:27:45 -0500

    fixed buttons and added js code

--- info-card/src/info-card.js
+++ info-card/src/info-card.js
@@ -1,5 +1,7 @@
 import { LitElement, html, css } from 'lit';
 
+const customImage = new URL('../assets/johncena.jpg', import.meta.url).href;
+
 export class InfoCard extends LitElement {
   static properties = {
     version: {},
@@ -8,79 +10,80 @@ export class InfoCard extends LitElement {
   static get styles() {
     return css`
       button {
-  padding: 8px 70px;
-  
-}
+      padding: 8px 70px;
+      background-color: blue;
+      }
 
-.headingchangebtn {
-  
-}
+      .headingchangebtn {
 
-.deletebtn{
-  
-}
+      }
 
-.backgroundbtn {
-  
-}
+      .deletebtn{
+
+      }
+
+      .backgroundbtn {
+
+      }
 
 
-.cardbtn {
-  background-color: lightblue;
-}
-.cardbtn:hover {
-  background-color: blue;
-}
+      .cardbtn {
+        background-color: lightblue;
+      }
+      .cardbtn:hover {
+        background-color: blue;
+      }
 
-.haxbtn {
-  visibility: hidden;
-  background-color: red;
-}
-.size {
-  border: 2px solid black;
-  background-color: grey;
-  max-width: 400px;
-  }
-@media screen and (min-width: 500px) and (max-width: 800px){
-  .haxbtn {
-    visibility: visible;
-    border: 8px solid black;
-  }
-}
-
-.top {
-  padding: 2px 50px;
-  background-color: lightgrey;
-}
-.image {
-  padding: 4px 100px;
-}
-.desc {
-  padding: 8px 20px;
-  border-style: solid hidden solid hidden;
-  background-color: lightgrey;
-  visibility: hidden;
-}
-img {width: 200px}
-
-@media screen and (max-width: 500px){
-  .size {
-    scale: 75%;
-    border: 5px solid black;
+      .haxbtn {
+        visibility: hidden;
+        background-color: red;
+      }
+      .size {
+        border: 2px solid black;
+        background-color: grey;
+        max-width: 400px;
+        }
       
-  }
-  .desc {
-    visibility: hidden;
-    border: 0px hidden;
-    padding: 8px 20px;
-  }
-  .image {
-    padding: 4px 10px;
-  }
-  .top {
-    padding: 4px 0px;
-  }
-}
+      /*@media screen and (min-width: 500px) and (max-width: 800px){
+        .haxbtn {
+          visibility: visible;
+          border: 8px solid black;
+        }
+      } */
+
+      .top {
+        padding: 2px 50px;
+        background-color: lightgrey;
+      }
+      .image {
+        padding: 4px 100px;
+      }
+      .desc {
+        padding: 8px 20px;
+        border-style: solid hidden solid hidden;
+        background-color: lightgrey;
+        visibility: hidden;
+      }
+      img {width: 200px}
+
+      @media screen and (max-width: 500px){
+        .size {
+          scale: 75%;
+          border: 5px solid black;
+
+        }
+        .desc {
+          visibility: hidden;
+          border: 0px hidden;
+          padding: 8px 20px;
+        }
+        .image {
+          padding: 4px 10px;
+        }
+        .top {
+          padding: 4px 0px;
+        }
+      }
     `;
   }
 
@@ -97,8 +100,8 @@ img {width: 200px}
           <h1> Thursday Homework (Week 2) </h1>
         </div>
         <div class="image">
-          <img src="https://i.ytimg.com/vi/0gC7onSsEUU/hqdefault.jpg" alt="Bing Chilling">
-          <button class="haxbtn"> Details </button>
+          <img src="${customImage}" alt="Bing Chilling">
+          
     
         </div>
         <div id="descript" class="desc">
